fc551777beb14fb3bcf03e22d6e5d97ad379f720
Al3xandr <Al3xandr.Goryunov@yandex.ru>
2025-01-20 12:50:19 +0300

    Initial Commit

---
+++ CryptoTab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoTab
+//
+//  Created by Alexandr on 20.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
